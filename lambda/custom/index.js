906732909d4333f27fe0bc43c2a24a353daf609d
kylethefox <halobuster1982>
2018-03-02 20:58:24 -0500

    debuging invalid response

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -13,26 +13,52 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
+const events = require('events');
+const redis = require("redis");
+const mysql = require('mysql');
 const questions = require('./question');
+const EventEmitter = events.EventEmitter;
 
+const APP_ID = "amzn1.ask.skill.05a79caf-aff3-40e1-83df-223bfaa410d3"; // TODO replace with your app ID (OPTIONAL)
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_TABLE = process.env.DB_TABLE;
+const DB_DATABASE = process.env.DB_DATABASE;
+const REDIS_HOST = process.env.REDIS_HOST;
+const NUMBER_OF_QUESTIONS = process.env.NUMBER_OF_QUESTIONS;
+
+const CACHE_KEY = 'QUESTIONS_KEY';
 const ANSWER_COUNT = 4; // The number of possible answers per trivia question.
-const GAME_LENGTH = 5;  // The number of questions per trivia game.
+const GAME_LENGTH = NUMBER_OF_QUESTIONS;  // The number of questions per trivia game.
 const GAME_STATES = {
     TRIVIA: '_TRIVIAMODE', // Asking trivia questions.
     START: '_STARTMODE', // Entry point, start the game.
     HELP: '_HELPMODE', // The user is asking for help.
 };
-const APP_ID = undefined; // TODO replace with your app ID (OPTIONAL)
+let langQuestions = {};
+
+const pool = mysql.createPool({
+    connectionLimit : 5, //important
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_DATABASE
+});
+
+const makeTextContent = Alexa.utils.TextUtils.makeTextContent;
+const makeRichText = Alexa.utils.TextUtils.makeRichText;
 
 /**
  * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
  * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
  */
-const languageString = {
+var languageString = {
     'en': {
         'translation': {
-            'QUESTIONS': questions['QUESTIONS_EN_US'],
-            'GAME_NAME': 'Reindeer Trivia', // Be sure to change this for your skill.
+            // 'QUESTIONS': questions['QUESTIONS_EN_US'],
+            // 'QUESTIONS': langQuestions['QUESTIONS_EN_US'],
+            'GAME_NAME': 'NCIS Trivia', // Be sure to change this for your skill.
             'HELP_MESSAGE': 'I will ask you %s multiple choice questions. Respond with the number of the answer. ' +
                 'For example, say one, two, three, or four. To start a new game at any time, say, start game. ',
             'REPEAT_QUESTION_MESSAGE': 'To repeat the last question, say, repeat. ',
@@ -58,43 +84,44 @@ const languageString = {
     },
     'en-US': {
         'translation': {
-            'QUESTIONS': questions['QUESTIONS_EN_US'],
-            'GAME_NAME': 'American Reindeer Trivia', // Be sure to change this for your skill.
+            // 'QUESTIONS': questions['QUESTIONS_EN_US'],
+            'GAME_NAME': 'NCIS Trivia', // Be sure to change this for your skill.
         },
     },
-    'en-GB': {
-        'translation': {
-            'QUESTIONS': questions['QUESTIONS_EN_GB'],
-            'GAME_NAME': 'British Reindeer Trivia', // Be sure to change this for your skill.
-        },
-    },
-    'de': {
-        'translation': {
-            'QUESTIONS': questions['QUESTIONS_DE_DE'],
-            'GAME_NAME': 'Wissenswertes über Rentiere in Deutsch', // Be sure to change this for your skill.
-            'HELP_MESSAGE': 'Ich stelle dir %s Multiple-Choice-Fragen. Antworte mit der Zahl, die zur richtigen Antwort gehört. ' +
-                'Sage beispielsweise eins, zwei, drei oder vier. Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“. ',
-            'REPEAT_QUESTION_MESSAGE': 'Wenn die letzte Frage wiederholt werden soll, sage „Wiederholen“ ',
-            'ASK_MESSAGE_START': 'Möchten Sie beginnen?',
-            'HELP_REPROMPT': 'Wenn du eine Frage beantworten willst, antworte mit der Zahl, die zur richtigen Antwort gehört. ',
-            'STOP_MESSAGE': 'Möchtest du weiterspielen?',
-            'CANCEL_MESSAGE': 'OK, dann lass uns bald mal wieder spielen.',
-            'NO_MESSAGE': 'OK, spielen wir ein andermal. Auf Wiedersehen!',
-            'TRIVIA_UNHANDLED': 'Sagt eine Zahl beispielsweise zwischen 1 und %s',
-            'HELP_UNHANDLED': 'Sage ja, um fortzufahren, oder nein, um das Spiel zu beenden.',
-            'START_UNHANDLED': 'Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“.',
-            'NEW_GAME_MESSAGE': 'Willkommen bei %s. ',
-            'WELCOME_MESSAGE': 'Ich stelle dir %s Fragen und du versuchst, so viele wie möglich richtig zu beantworten. ' +
-            'Sage einfach die Zahl, die zur richtigen Antwort passt. Fangen wir an. ',
-            'ANSWER_CORRECT_MESSAGE': 'Richtig. ',
-            'ANSWER_WRONG_MESSAGE': 'Falsch. ',
-            'CORRECT_ANSWER_MESSAGE': 'Die richtige Antwort ist %s: %s. ',
-            'ANSWER_IS_MESSAGE': 'Diese Antwort ist ',
-            'TELL_QUESTION_MESSAGE': 'Frage %s. %s ',
-            'GAME_OVER_MESSAGE': 'Du hast %s von %s richtig beantwortet. Danke fürs Mitspielen!',
-            'SCORE_IS_MESSAGE': 'Dein Ergebnis ist %s. ',
-        },
-    },
+    // 'en-GB': {
+    //     'translation': {
+    //         // 'QUESTIONS': questions['QUESTIONS_EN_GB'],
+    //         // 'QUESTIONS': langQuestions['QUESTIONS_EN_GB'],
+    //         'GAME_NAME': 'British Reindeer Trivia', // Be sure to change this for your skill.
+    //     },
+    // },
+    // 'de': {
+    //     'translation': {
+    //         'QUESTIONS': questions['QUESTIONS_DE_DE'],
+    //         'GAME_NAME': 'Wissenswertes über Rentiere in Deutsch', // Be sure to change this for your skill.
+    //         'HELP_MESSAGE': 'Ich stelle dir %s Multiple-Choice-Fragen. Antworte mit der Zahl, die zur richtigen Antwort gehört. ' +
+    //             'Sage beispielsweise eins, zwei, drei oder vier. Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“. ',
+    //         'REPEAT_QUESTION_MESSAGE': 'Wenn die letzte Frage wiederholt werden soll, sage „Wiederholen“ ',
+    //         'ASK_MESSAGE_START': 'Möchten Sie beginnen?',
+    //         'HELP_REPROMPT': 'Wenn du eine Frage beantworten willst, antworte mit der Zahl, die zur richtigen Antwort gehört. ',
+    //         'STOP_MESSAGE': 'Möchtest du weiterspielen?',
+    //         'CANCEL_MESSAGE': 'OK, dann lass uns bald mal wieder spielen.',
+    //         'NO_MESSAGE': 'OK, spielen wir ein andermal. Auf Wiedersehen!',
+    //         'TRIVIA_UNHANDLED': 'Sagt eine Zahl beispielsweise zwischen 1 und %s',
+    //         'HELP_UNHANDLED': 'Sage ja, um fortzufahren, oder nein, um das Spiel zu beenden.',
+    //         'START_UNHANDLED': 'Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“.',
+    //         'NEW_GAME_MESSAGE': 'Willkommen bei %s. ',
+    //         'WELCOME_MESSAGE': 'Ich stelle dir %s Fragen und du versuchst, so viele wie möglich richtig zu beantworten. ' +
+    //         'Sage einfach die Zahl, die zur richtigen Antwort passt. Fangen wir an. ',
+    //         'ANSWER_CORRECT_MESSAGE': 'Richtig. ',
+    //         'ANSWER_WRONG_MESSAGE': 'Falsch. ',
+    //         'CORRECT_ANSWER_MESSAGE': 'Die richtige Antwort ist %s: %s. ',
+    //         'ANSWER_IS_MESSAGE': 'Diese Antwort ist ',
+    //         'TELL_QUESTION_MESSAGE': 'Frage %s. %s ',
+    //         'GAME_OVER_MESSAGE': 'Du hast %s von %s richtig beantwortet. Danke fürs Mitspielen!',
+    //         'SCORE_IS_MESSAGE': 'Dein Ergebnis ist %s. ',
+    //     },
+    // },
 };
 
 const newSessionHandlers = {
@@ -110,9 +137,14 @@ const newSessionHandlers = {
         this.handler.state = GAME_STATES.HELP;
         this.emitWithState('helpTheUser', true);
     },
+    'LaunchIntent' : function() {
+        this.handler.state = GAME_STATES.START;
+        this.emitWithState('StartGame', true);
+    },
     'Unhandled': function () {
         const speechOutput = this.t('START_UNHANDLED');
         this.response.speak(speechOutput).listen(speechOutput);
+        this.response.cardRenderer(this.t('GAME_NAME'), speechOutput);
         this.emit(':responseReady');
     },
 };
@@ -152,6 +184,9 @@ function populateGameQuestions(translatedQuestions) {
 function populateRoundAnswers(gameQuestionIndexes, correctAnswerIndex, correctAnswerTargetLocation, translatedQuestions) {
     const answers = [];
     const answersCopy = translatedQuestions[gameQuestionIndexes[correctAnswerIndex]][Object.keys(translatedQuestions[gameQuestionIndexes[correctAnswerIndex]])[0]].slice();
+    
+    console.log('answersCopy: ' + answersCopy);
+    
     let index = answersCopy.length;
 
     if (index < ANSWER_COUNT) {
@@ -224,7 +259,7 @@ function handleUserGuess(userGaveUp) {
         let repromptText = this.t('TELL_QUESTION_MESSAGE', questionIndexForSpeech.toString(), spokenQuestion);
 
         for (let i = 0; i < ANSWER_COUNT; i++) {
-            repromptText += `${i + 1}. ${roundAnswers[i]}. `;
+            repromptText += '<br/>' + '<font size="5">' + `${i + 1}. ${roundAnswers[i]}. ` + '</font>';
         }
 
         speechOutput += userGaveUp ? '' : this.t('ANSWER_IS_MESSAGE');
@@ -240,8 +275,15 @@ function handleUserGuess(userGaveUp) {
             'correctAnswerText': translatedQuestions[gameQuestions[currentQuestionIndex]][Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0]][0],
         });
 
+        const builder = new Alexa.templateBuilders.BodyTemplate1Builder();
+        let template = builder.setTitle(this.t('GAME_NAME'))
+                          .setTextContent(makeRichText(repromptText))
+                          .build();
+        
+
         this.response.speak(speechOutput).listen(repromptText);
-        this.response.cardRenderer(this.t('GAME_NAME', repromptText));
+        this.response.cardRenderer(this.t('GAME_NAME'), repromptText);
+        this.response.renderTemplate(template);
         this.emit(':responseReady');
     }
 }
@@ -252,6 +294,7 @@ const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
         // Select GAME_LENGTH questions for the game
         const translatedQuestions = this.t('QUESTIONS');
         const gameQuestions = populateGameQuestions(translatedQuestions);
+
         // Generate a random index for the correct answer, from 0 to 3
         const correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
         // Select and shuffle the answers for each question
@@ -259,8 +302,10 @@ const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
         const currentQuestionIndex = 0;
         const spokenQuestion = Object.keys(translatedQuestions[gameQuestions[currentQuestionIndex]])[0];
         let repromptText = this.t('TELL_QUESTION_MESSAGE', '1', spokenQuestion);
+        let displayText = this.t('TELL_QUESTION_MESSAGE', '1', spokenQuestion);
 
         for (let i = 0; i < ANSWER_COUNT; i++) {
+            displayText += '<br/>' + '<font size="5">' + `${i + 1}. ${roundAnswers[i]}. ` + '</font>';
             repromptText += `${i + 1}. ${roundAnswers[i]}. `;
         }
 
@@ -279,8 +324,18 @@ const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
         // Set the current state to trivia mode. The skill will now use handlers defined in triviaStateHandlers
         this.handler.state = GAME_STATES.TRIVIA;
 
+        const builder = new Alexa.templateBuilders.BodyTemplate1Builder();
+        let template = builder.setTitle(this.t('GAME_NAME'))
+                          .setTextContent(makeRichText(displayText))
+                          .build();
+
+        console.log(speechOutput);
+        console.log(speechOutput.replace(new RegExp('/(<.*?>)/', 'g')));
         this.response.speak(speechOutput).listen(repromptText);
-        this.response.cardRenderer(this.t('GAME_NAME'), repromptText);
+        // this.response.cardRenderer(this.t('GAME_NAME'), displayText);
+        this.response.renderTemplate(template);
+
+        console.log('Just about to emit start response');
         this.emit(':responseReady');
     },
 });
@@ -297,7 +352,14 @@ const triviaStateHandlers = Alexa.CreateStateHandler(GAME_STATES.TRIVIA, {
         this.emitWithState('StartGame', false);
     },
     'AMAZON.RepeatIntent': function () {
+        const builder = new Alexa.templateBuilders.BodyTemplate1Builder();
+        let template = builder.setTitle(this.t('GAME_NAME'))
+                          .setTextContent(makeRichText(this.attributes['speechOutput']))
+                          .build();
+
         this.response.speak(this.attributes['speechOutput']).listen(this.attributes['repromptText']);
+        this.response.cardRenderer(this.t('GAME_NAME'), this.attributes['speechOutput']);
+        this.response.renderTemplate(template);
         this.emit(':responseReady');
     },
     'AMAZON.HelpIntent': function () {
@@ -308,6 +370,7 @@ const triviaStateHandlers = Alexa.CreateStateHandler(GAME_STATES.TRIVIA, {
         this.handler.state = GAME_STATES.HELP;
         const speechOutput = this.t('STOP_MESSAGE');
         this.response.speak(speechOutput).listen(speechOutput);
+        this.response.cardRenderer(this.t('GAME_NAME'), speechOutput);
         this.emit(':responseReady');
     },
     'AMAZON.CancelIntent': function () {
@@ -317,6 +380,7 @@ const triviaStateHandlers = Alexa.CreateStateHandler(GAME_STATES.TRIVIA, {
     'Unhandled': function () {
         const speechOutput = this.t('TRIVIA_UNHANDLED', ANSWER_COUNT.toString());
         this.response.speak(speechOutput).listen(speechOutput);
+        this.response.cardRenderer(this.t('GAME_NAME'), speechOutput);
         this.emit(':responseReady');
     },
     'SessionEndedRequest': function () {
@@ -331,6 +395,7 @@ const helpStateHandlers = Alexa.CreateStateHandler(GAME_STATES.HELP, {
         const repromptText = this.t('HELP_REPROMPT') + askMessage;
 
         this.response.speak(speechOutput).listen(repromptText);
+        this.response.cardRenderer(this.t('GAME_NAME'), repromptText);
         this.emit(':responseReady');
     },
     'AMAZON.StartOverIntent': function () {
@@ -357,20 +422,24 @@ const helpStateHandlers = Alexa.CreateStateHandler(GAME_STATES.HELP, {
     'AMAZON.NoIntent': function () {
         const speechOutput = this.t('NO_MESSAGE');
         this.response.speak(speechOutput);
+        this.response.cardRenderer(this.t('GAME_NAME'), speechOutput);
         this.emit(':responseReady');
     },
     'AMAZON.StopIntent': function () {
         const speechOutput = this.t('STOP_MESSAGE');
         this.response.speak(speechOutput).listen(speechOutput);
+        this.response.cardRenderer(this.t('GAME_NAME'), speechOutput);
         this.emit(':responseReady');
     },
     'AMAZON.CancelIntent': function () {
         this.response.speak(this.t('CANCEL_MESSAGE'));
+        this.response.cardRenderer(this.t('GAME_NAME'), this.t('CANCEL_MESSAGE'));
         this.emit(':responseReady');
     },
     'Unhandled': function () {
         const speechOutput = this.t('HELP_UNHANDLED');
         this.response.speak(speechOutput).listen(speechOutput);
+        this.response.cardRenderer(this.t('GAME_NAME'), speechOutput);
         this.emit(':responseReady');
     },
     'SessionEndedRequest': function () {
@@ -378,11 +447,86 @@ const helpStateHandlers = Alexa.CreateStateHandler(GAME_STATES.HELP, {
     },
 });
 
-exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageString;
-    alexa.registerHandlers(newSessionHandlers, startStateHandlers, triviaStateHandlers, helpStateHandlers);
-    alexa.execute();
+exports.handler = function (event, context, callback) {
+    var flowController = new EventEmitter();
+    var client = redis.createClient(REDIS_HOST);
+
+    context.callbackWaitsForEmptyEventLoop = false; 
+
+    client.on("error", function (err) {
+        console.log("Error " + err);
+    });
+
+    flowController.on('getCacheResults', () => {
+        client.get("CACHE_KEY", (err, reply) => {
+            if (reply !== null) {
+                console.log('Found in cache. SUPER SPEED!!!');
+                flowController.emit('startAlexa', JSON.parse(reply));
+            } else {
+                console.log('cache empty, going to database');
+                flowController.emit('callDatabase');
+            }
+            
+        })
+
+    });
+      
+    flowController.on('callDatabase', () => {
+        pool.getConnection((err,connection) => {
+            if (err) {
+                throw err;
+                connection.release();
+                res.json({"code" : 100, "status" : "Error in connection database"});
+                return;
+            }   
+
+            connection.query("SELECT * FROM " + DB_TABLE + " where active_ind = 'Y'", (err,rows) => {
+
+                if(err) throw err;
+                connection.release();
+
+                langQuestions = {};
+
+                rows.forEach(element => {
+                    var questionNode = {}
+                    questionNode[element.question_txt] = new Array();
+                    questionNode[element.question_txt].push(element.ans_1);
+                    questionNode[element.question_txt].push(element.ans_2);
+                    questionNode[element.question_txt].push(element.ans_3);
+                    questionNode[element.question_txt].push(element.ans_4);
+
+                    if (typeof langQuestions['QUESTIONS_' + element.language] === 'undefined') {
+                        langQuestions['QUESTIONS_' + element.language] = new Array();
+                    }
+
+                    langQuestions['QUESTIONS_' + element.language].push(questionNode);
+                });
+
+                
+
+                languageString.en.translation['QUESTIONS'] = langQuestions['QUESTIONS_EN_US'];
+                languageString['en-US'].translation.QUESTIONS = langQuestions['QUESTIONS_EN_US'];
+                // languageString['en-GB'].translation.QUESTIONS = langQuestions['QUESTIONS_EN_GB'];
+
+                client.set("CACHE_KEY", JSON.stringify(languageString));
+                flowController.emit('startAlexa', languageString);
+                
+            });
+        });
+    });
+
+    flowController.on('startAlexa', (langString) => {
+        console.log('Starting Application');
+        client.quit();
+        const alexa = Alexa.handler(event, context);
+        alexa.appId = APP_ID;
+        // To enable string internationalization (i18n) features, set a resources object.
+        // alexa.resources = languageString;
+        alexa.resources = langString;
+        alexa.registerHandlers(newSessionHandlers, startStateHandlers, triviaStateHandlers, helpStateHandlers);
+        alexa.execute();
+    });
+
+    flowController.emit('getCacheResults');
+    // flowController.emit('startAlexa', languageString);
 };
